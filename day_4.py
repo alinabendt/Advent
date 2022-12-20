7fc80436306664532daa306bbdd0f9131f9c98b8
alinabendt <81580693+alinabendt@users.noreply.github.com>
2022-12-20 11:27:12 +0100

    day 4 complete

--- day_4.py
+++ day_4.py
@@ -21,14 +21,13 @@ with open('/mnt/c/Users/Uni/Documents/GitHub/Advent/input_day4.txt', 'r') as fil
         pair = [int(s) for s in re.findall(r'\d+', line)]
         if pair[0] == pair[2] == pair[1] == pair[3]:
             pass
-        elif pair[0] <= pair[2] and pair[1] <= pair[3]:
+        if pair[0] <= pair[2] and pair[1] >= pair[3]:  # special case pair 2 completely in pair 1
             counter +=1
-        elif pair[0] >= pair[2] and pair[1] >= pair[3]:
+        elif pair[0] >= pair[2] and pair[1] <= pair[3]:  # special case pair 1 completely in pair 2
             counter +=1
-        elif pair[0] <= pair[2] and pair[1] >= pair[3]:
+        elif pair[0] <= pair[2] <= pair[1] <= pair[3]:  # example 2 < 3 < 5 <= 5  pair: 2-5,3-5
             counter +=1
-        elif pair[0] >= pair[2] and pair[1] <= pair[3]:
+        elif pair[1] >= pair[3] >= pair[0] >= pair[2]:  # example 7 > 6 > 5 > 3 pair: 7-5,6-3
             counter +=1
-
 print(counter)
 
