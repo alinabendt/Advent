bcd9405aad267ffc1a4ec19a85ffc4df1c90d7c8
alinabendt <81580693+alinabendt@users.noreply.github.com>
2022-12-23 13:03:21 +0100

    day 7 part 1 works
    use path instead of name and parent due to repeated use of same folder names

--- day_7.py
+++ day_7.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-directories = {'/': {'parent': '/', 'size':[], 'daughters': []}}
-def add_dir(parent, directory):
-    directories[directory] = {}
-    directories[directory]['parent'] = parent
-    directories[directory]['size'] = []
-    directories[directory]['daughters'] = []
-    directories[parent]['daughters'] = np.append(directories.get(parent).get('daughters'), directory)
+directories = {'/': {'size':[], 'daughters': [], 'updated': [], 'level': 0}}
+def add_dir(path, directory):
+    name = path+'-'+directory
+    directories[name] = {}
+    directories[name]['size'] = []
+    directories[name]['daughters'] = []
+    directories[path]['daughters'] = np.append(directories.get(path).get('daughters'), directory)
+    directories[name]['updated'] = []
+    directories[name]['level'] = 0
 
-def add_file(directory, file_size):
-    directories[directory]['size'] = np.unique(np.append(directories.get(directory).get('size'), file_size))
+
+def add_file(path, file_size):
+    directories[path]['size'] = np.append(directories.get(path).get('size'), file_size)
 
 
 def navigate(command, current):
@@ -21,18 +24,27 @@ def navigate(command, current):
             pass 
         elif command[1] == 'cd':
             if '.' in command[2]:
-                parent = directories.get(current).get('parent')
-                current = parent
-                # print('go up to', current)
+                if current == '/':
+                    pass
+                else:
+                    parent = current.rsplit('-', 1)[0]
+                    current = parent
+                    # print('go up to', current)
+            elif command[2] == '/':
+                current = '/'
             else:
                 folder = command[2]
-                current = folder
-                # print('go in to', folder)
+                new = current+'-'+folder
+                # print('go in from ', current, 'to', new)
+                current = new
     elif command.startswith('dir'):
         name = command.split(' ')[1]
-        if name not in list(directories.keys()):
+        fname = current+'-'+name
+        if fname not in list(directories.keys()):
             add_dir(current, name)
-        # print('add dir', name, current)
+            # print('added directory', name)
+        else:
+            print('directory already exists with', current+'-'+name)
     elif command[0].isdigit():
         folder = current
         size = int(command.split(' ')[0])
@@ -45,29 +57,38 @@ def update_size(folder):
     return directories
 
 def update_parent(folder):
-    parent = directories.get(folder).get('parent')
+    parent = folder.rsplit('-', 1)[0]
+    directories.get(parent).get('updated').append(folder)
     add_file(parent, directories.get(folder).get('size'))
     return parent
 
+def assign_level(folder, level):
+    for d in directories.get(folder).get('daughters'):
+        da = folder+'-'+d
+        directories[da]['level'] = level+1
+    level += 1
+    return level
+
 with open('/mnt/c/Users/Uni/Documents/GitHub/Advent/input_day7.txt', 'r') as file:
-    c_folder = '/'
+    c_folder = '/-/'
     for line in file:
         c_folder = navigate(line, c_folder)
 
 # determine folders with size <= 100,000
 len0 = []
+level = 0
 for f in list(directories.keys()):
+    level = assign_level(f, level)
     if len(directories.get(f).get('daughters')) == 0:
         len0.append(f)
-for f in len0:
-    folder = f
-    while folder != '/':
-        parent = update_parent(folder)
-        folder = parent
-print('updated parents:', directories)
-for folder in list(reversed(directories.keys())):
+maxs = [directories.get(key).get('level') for key in directories]
+max_level = max(maxs)
+# sort directories by level
+directories = dict(sorted(directories.items(), key=lambda item: item[1]['level']))
+for f in list(reversed(directories.keys()))[0:-1]:  # update parent folder size from lowest level up to /
+    parent = update_parent(f)
+for folder in list(reversed(directories.keys())):  # update total size of folders
     directories = update_size(folder)
-print('summed:', directories)
 
 smalls = []
 total = []
