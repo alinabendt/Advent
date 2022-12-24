bd95271df146c207450afb9ab5b3a71f00b825c3
alinabendt <81580693+alinabendt@users.noreply.github.com>
2022-12-24 17:08:31 +0100

    day 9 complete

--- day_9.py
+++ day_9.py
@@ -1,45 +1,103 @@
 import numpy as np
 
-grid = np.zeros(10,10)
-grid[0,0] = 'H'
+class heta():
+    history = [[0, 0]]
+    def __init__(self, pos):
+        self.pos = pos
 
-def move_head(m, pos, pos_t):
-    steps = m[1]
-    for s in range(m[1]):
-        if m[0] == 'R':
-            grid[pos] = 0
-            pos[1] = pos[1]+steps
-        elif m[0] == 'L':
-            grid[pos] = 0
-            pos[1] = pos[1]-steps
-        elif m[0] == 'U':
-            grid[pos] = 0
-            pos[0] = pos[0]+steps
-        elif m[0] == 'D':
-            grid[pos] = 0
-            pos[0] = pos[0]-steps
-        if pos[0]<0 or pos[1]<0 or pos[0]>np.shape(grid)[0] or pos[1]>np.shape(grid)[1]:
-            grid = extend(pos)
-        grid[pos] = 'H'
-        move_tail(pos, pos_t)
-    return pos, pos_t
+    def move(self, steps):
+        pos_h = self.pos
+        if steps[0] == 'R':
+            pos_h[1] = self.pos[1]+1
+        elif steps[0] == 'L':
+            pos_h[1] = self.pos[1]-1
+        elif steps[0] == 'U':
+            pos_h[0] = self.pos[0]-1
+        elif steps[0] == 'D':
+            pos_h[0] = self.pos[0]+1
+        self.pos = pos_h
+        self.history = np.append(self.history, [self.pos], axis=0)
+        return pos_h
+    def move_t(self, head):
+        if head[1] == self.pos[1]:  # head and tail in same column
+            if head[0]-self.pos[0] == -2:  # head is two steps above
+                self.pos[0] = self.pos[0]-1
+            elif head[0]-self.pos[0] == 2:  # head is two steps below
+                self.pos[0] = self.pos[0]+1
+        elif head[0] == self.pos[0]:  # head and tail in same row
+            if head[1]-self.pos[1] == -2:  # head is two steps left
+                self.pos[1] = self.pos[1]-1
+            elif head[1]-self.pos[1] == 2:  # head is two steps right
+                self.pos[1] = self.pos[1]+1
+        # diagonal cases
+        elif head[1]-self.pos[1] == 2 and head[0]-self.pos[0] == -1:
+            self.pos = [self.pos[0]-1, self.pos[1]+1]
+        elif head[1]-self.pos[1] == 2 and head[0]-self.pos[0] == 1:
+            self.pos = [self.pos[0]+1, self.pos[1]+1]
+        elif head[1]-self.pos[1] == -2 and head[0]-self.pos[0] == -1:
+            self.pos = [self.pos[0]-1, self.pos[1]-1]
+        elif head[1]-self.pos[1] == -2 and head[0]-self.pos[0] == 1:
+            self.pos = [self.pos[0]+1, self.pos[1]-1]
+        elif head[0]-self.pos[0] == 2 and head[1]-self.pos[1] == -1:
+            self.pos = [self.pos[0]+1, self.pos[1]-1]
+        elif head[0]-self.pos[0] == 2 and head[1]-self.pos[1] == 1:
+            self.pos = [self.pos[0]+1, self.pos[1]+1]
+        elif head[0]-self.pos[0] == -2 and head[1]-self.pos[1] == -1:
+            self.pos = [self.pos[0]-1, self.pos[1]-1]
+        elif head[0]-self.pos[0] == -2 and head[1]-self.pos[1] == 1:
+            self.pos = [self.pos[0]-1, self.pos[1]+1]
+        elif head[0]-self.pos[0] == 2 and head[1]-self.pos[1] == 2:
+            self.pos = [self.pos[0]+1, self.pos[1]+1]
+        elif head[0]-self.pos[0] == -2 and head[1]-self.pos[1] == -2:
+            self.pos = [self.pos[0]-1, self.pos[1]-1]
+        elif head[0]-self.pos[0] == 2 and head[1]-self.pos[1] == 2:
+            self.pos = [self.pos[0]+1, self.pos[1]+1]
+        elif head[0]-self.pos[0] == -2 and head[1]-self.pos[1] == 2:
+            self.pos = [self.pos[0]-1, self.pos[1]+1]
+        elif head[0]-self.pos[0] == 2 and head[1]-self.pos[1] == -2:
+            self.pos = [self.pos[0]+1, self.pos[1]-1]
+        self.history = np.append(self.history, [self.pos], axis=0)
+        return self.pos
 
-def move_tail(pos_h, pos_t):
-    if pos_h[0]
-
-def extend(position):
-    if position[0]<0:
-        grid = np.insert(np.zeros((1, np.shape(grid)[1])), 0)
-    if position[1]<0:
-        grid = np.insert(np.zeros((np.shape(grid)[0], 1)), 0)
-    if position[0]>np.shape(grid)[0]:
-        grid = np.append(grid, np.zeros((1, np.shape(grid)[1])), axis=0)
-    if position[1]>np.shape(grid)[1]:
-        grid = np.append(grid, np.zeros((np.shape(grid)[0], 1)), axis=1)
-    return grid
+Head = heta([0,0])
+Tail = heta([0,0])
 
 with open('/mnt/c/Users/Uni/Documents/GitHub/Advent/input_day9.txt', 'r') as file:
     for line in file:
-        command = line.strip()
-        command = line.split(' ')
-        grid = move(command)
+        command = line.strip('\n')
+        command = command.split(' ')
+        for _ in range(int(command[1])):
+            Head.pos = Head.move(command)
+            Tail.pos = Tail.move_t(Head.pos)
+no_tail = np.shape(np.unique(Tail.history, axis=0))[0]
+print('the tail visited ', no_tail, ' positions')
+
+# part 2
+Head = heta([0,0])
+Knot1 = heta([0,0])
+Knot2 = heta([0,0])
+Knot3 = heta([0,0])
+Knot4 = heta([0,0])
+Knot5 = heta([0,0])
+Knot6 = heta([0,0])
+Knot7 = heta([0,0])
+Knot8 = heta([0,0])
+Knot9 = heta([0,0])
+with open('/mnt/c/Users/Uni/Documents/GitHub/Advent/input_day9.txt', 'r') as file:
+    for line in file:
+        command = line.strip('\n')
+        command = command.split(' ')
+        for _ in range(int(command[1])):
+            Head.pos = Head.move(command)
+            Knot1.pos = Knot1.move_t(Head.pos)
+            Knot2.pos = Knot2.move_t(Knot1.pos)
+            Knot3.pos = Knot3.move_t(Knot2.pos)
+            Knot4.pos = Knot4.move_t(Knot3.pos)
+            Knot5.pos = Knot5.move_t(Knot4.pos)
+            Knot6.pos = Knot6.move_t(Knot5.pos)
+            Knot7.pos = Knot7.move_t(Knot6.pos)
+            Knot8.pos = Knot8.move_t(Knot7.pos)
+            Knot9.pos = Knot9.move_t(Knot8.pos)
+no_tail = np.shape(np.unique(Knot9.history, axis=0))[0]
+print('the Knot9 visited ', no_tail, ' positions')
+
