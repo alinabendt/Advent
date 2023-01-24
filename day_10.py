bd2a60812dabb9a055b7866f6dea34d580695062
alinabendt <alina-bendt@gmx.net>
2023-01-24 16:52:46 +0000

    completes day 10

--- day_10.py
+++ day_10.py
@@ -22,9 +22,9 @@ def execute(strength, c, x, com):
             c += 1
         x += [int(d) for d in re.findall(r'-?\d+', com[1])][0]
     return strength, c, x
-        
 
-with open('/mnt/c/Users/Uni/Documents/GitHub/Advent/input_day10.txt', 'r') as file:
+
+with open('/home/alina/Advent/input_day10.txt', 'r') as file:
     for line in file:
         line = line.strip('\n')
         line = line.split(' ')
@@ -34,3 +34,51 @@ s = [strength.get(key) for key in strength.keys()]
 print('sum of signal strengths: ', np.sum(s))
 
 # part 2
+def sprite(x):
+    pos = [x-1, x, x+1]
+    return pos
+
+
+def draw(x, c_line):
+    sprite_pos = sprite(x)
+    if len(c_line) in sprite_pos:
+        c_line = c_line+"#"
+    else:
+        c_line = c_line+"."
+    return c_line
+
+
+def execute2(strength, c, x, com, c_line, display):
+    if com[0] == 'noop':
+        strength = check_cycle(strength, c, x)
+        c_line, display = check_line(c_line, display)
+        c_line= draw(x, c_line)
+        c += 1
+    else:
+        for _ in range(2):
+            strength = check_cycle(strength, c, x)
+            c_line, display = check_line(c_line, display)
+            c_line = draw(x, c_line)
+            c += 1
+        x += [int(d) for d in re.findall(r'-?\d+', com[1])][0]
+    return c_line, strength, c, x, display
+
+
+def check_line(c_line, display):
+    if len(c_line) == 40:
+        display.append(c_line)
+        c_line = str()
+    return c_line, display
+
+
+with open('/home/alina/Advent/input_day10.txt', 'r') as file:
+    display = []
+    c_line = str()
+    xs = 1
+    for line in file:
+        line = line.strip('\n')
+        line = line.split(' ')
+        c_line, strength, cycle, xs, display = execute2(strength, cycle, xs, line, c_line, display)
+        c_line, display = check_line(c_line, display)
+for i in range(np.size(display)):
+    print(display[i])
